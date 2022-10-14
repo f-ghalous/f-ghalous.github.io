ee46175859898d9d011cc2e110604952905d2b87
f-ghalous <111303122+f-ghalous@users.noreply.github.com>
2022-10-13 22:10:52 -0400

    brassage terminer

--- labo2/carte.js
+++ labo2/carte.js
@@ -11,33 +11,6 @@ var images = ["image/1.gif", "image/2.gif","image/3.gif",
 "image/44.gif","image/45.gif","image/46.gif","image/47.gif",
 "image/48.gif","image/49.gif","image/50.gif", "image/51.gif", "image/52.gif"];
 
-function generationCartes(){
-    const COULEURS = ["♦", "♣", "♥", "♠"];
-    const VALEURS = ["A"];
-    const myArray2 = ["V", "D", "R"];
-    for(var i=2; i < 11; i++){
-        VALEURS.push(i);
-    }
-    VALEURS.push.apply(VALEURS, myArray2);
-    const cartes = [];0
-    for(var i=0; i< COULEURS.length; i++){
-        for(var n=0; n < VALEURS.length; n++){
-            cartes.push(COULEURS[i]+VALEURS[n]);
-        }    
-    }
-    return cartes}
-
-/*function  afficherCartes(carte){
-    let resultat = document.getElementById("demo");
-    resultat.innerHTML = "";
-    for (let i = 0; i < 4; i++){
-        resultat.innerHTML += "<div>"
-        for (let j=0; j<13; j++){
-            c = carte[13*i+j]
-            resultat.innerHTML += c 
-            }
-        resultat.innerHTML += "</div>";}}*/
-
 function  afficherCartes(){
     let resultat = document.getElementById("demo");
     resultat.innerHTML = "";
@@ -48,34 +21,17 @@ function  afficherCartes(){
                     }
         resultat.innerHTML += "</div>";}       
 
-
 let button = document.getElementById("brassage");
-//let carte = generationCartes();
-
-/*var brassage = function(){
-    let resultat = document.getElementById("demo");
-    const moitie_deck = carte.length / 2;
-    const nouveau_paquet = [];
-    for (var i = 0; i < moitie_deck; i++){
-        nouveau_paquet.push(carte[i]);
-        nouveau_paquet.push(carte[i+moitie_deck]); }   
-    carte = nouveau_paquet;
-    afficherCartes(carte);}  */
-
 var brassage = function(){
-   // let resultat = document.getElementById("demo");
-    const moitie_deck = images.length / 2;
-    const nouveau_paquet = [];
-    for (var i = 0; i < moitie_deck; i++){
-        nouveau_paquet.push(images[i]);
-        nouveau_paquet.push(images[i+moitie_deck]); }   
-        let carte = nouveau_paquet;
-        afficherCartes(carte);}  
-
+    const moitieDeck = images.length / 2;
+    const nouveauPaquet = [];
+    for (var i = 0; i < moitieDeck; i++){
+        nouveauPaquet.push(images[i]);
+        nouveauPaquet.push(images[i+moitieDeck]); }   
+        images = nouveauPaquet;
+        afficherCartes();}  
 
 button.addEventListener('click', brassage);
 
-//let carte1 = generationCartes();
-//console.log(carte1);
 afficherCartes();       
 
