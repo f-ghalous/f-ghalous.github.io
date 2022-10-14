c87e6e6f1fc0760928c197b5aab92b9682433370
f-ghalous <111303122+f-ghalous@users.noreply.github.com>
2022-10-13 22:21:09 -0400

    dernier update

--- labo2/carte.js
+++ labo2/carte.js
@@ -1,7 +1,7 @@
 var images = ["image/1.gif", "image/2.gif","image/3.gif",
 "image/4.gif","image/5.gif","image/6.gif","image/7.gif",
 "image/8.gif","image/9.gif","image/10.gif","image/11.gif",
-"image/12.gif.","image/13.gif","image/14.gif","image/15.gif",
+"image/12.gif","image/13.gif","image/14.gif","image/15.gif",
 "image/16.gif","image/17.gif","image/18.gif","image/19.gif",
 "image/20.gif","image/21.gif","image/22.gif","image/23.gif",
 "image/24.gif","image/25.gif","image/26.gif","image/27.gif",
