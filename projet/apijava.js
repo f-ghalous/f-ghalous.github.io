37c203a01cbfb19cb2760ccc32a518bdac81d2e2
f-ghalous <111303122+f-ghalous@users.noreply.github.com>
2022-10-23 18:49:07 -0400

    update

--- projet/apijava.js
+++ projet/apijava.js
@@ -10,11 +10,11 @@ async function get(){
     document.querySelector("#content2 h1").innerHTML = data[4].name;
     document.querySelector("#content2 h2").innerHTML = data[4].occupation;
     document.querySelector("#content2 img").src = data[4].img;
-    document.querySelector("#content3 h1").innerHTML = data[21].name;
-    document.querySelector("#content3 h2").innerHTML = data[21].occupation;
-    document.querySelector("#content3 img").src = data[21].img;
+    document.querySelector("#content3 h1").innerHTML = data[27].name;
+    document.querySelector("#content3 h2").innerHTML = data[27].occupation;
+    document.querySelector("#content3 img").src = data[27].img;
     document.querySelector("#content4 h1").innerHTML = data[5].name;
     document.querySelector("#content4 h2").innerHTML = data[5].occupation;
     document.querySelector("#content4 img").src = data[5].img;
-    }
+}
 get();
