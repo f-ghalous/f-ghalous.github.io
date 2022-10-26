3ad04988b90408f6d36138f30435a7b9a3523ddf
f-ghalous <111303122+f-ghalous@users.noreply.github.com>
2022-10-25 20:15:47 -0400

    update

--- projet/java.js
+++ projet/java.js
@@ -6,18 +6,18 @@ button.addEventListener("click", ()=>{
     let taille = parseInt(document.getElementById("taille").value);
     let poids = parseInt(document.getElementById("poids").value);
     let resultat = document.getElementById("output");
-    let result_taille = false, result_poids = false;
-if(taille === "" || isNaN(taille) || (taille <= 0)){
-    document.getElementById("taille_erreur").innerHTML = "taille incorrect, veuillez donner une taille en cm";
+    //let resultaTaille = false, resultApoids = false;
+/*if(taille === "" || (taille <= 0)){
+    document.getElementById("tailleErreur").innerHTML = "taille incorrect, veuillez donner une taille en cm";
 } else{document.getElementById("taille_erreur").innerHTML = "";
-result_taille = true;
+resultasTaille = true;
 }
 if(poids === "" || isNaN(poids) || (poids <= 0)){
     document.getElementById("poids_erreur").innerHTML = "poids incorrect, veuillez donner un poids en kg";
 } else{document.getElementById("poids_erreur").innerHTML = "";
 result_poids = true;
 }
-if(result_poids && result_taille){
+if(result_poids && result_taille){*/
     const imc = (poids/ ((taille * taille)/10000)).toFixed(2);
     if(imc < 18.5){
         resultat.innerHTML = "Maigreur:" +""+ imc;
