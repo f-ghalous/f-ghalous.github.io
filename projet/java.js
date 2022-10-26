e90c0a926df102fed60c36f79a0ce372dd201837
f-ghalous <111303122+f-ghalous@users.noreply.github.com>
2022-10-26 16:28:20 -0400

    update imc

--- projet/java.js
+++ projet/java.js
@@ -1,33 +1,20 @@
-// clculate imc
-// inspirer d'une chaine youtub indienne.
-button = document.getElementById("imc");
+// calculer imc
 
-button.addEventListener("click", ()=>{
-    let taille = parseInt(document.getElementById("taille").value);
-    let poids = parseInt(document.getElementById("poids").value);
+function calculeImc(){
+    let taille = parseFloat(document.getElementById("taille").value);
+    let poids = parseFloat(document.getElementById("poids").value);
     let resultat = document.getElementById("output");
-    //let resultaTaille = false, resultApoids = false;
-/*if(taille === "" || (taille <= 0)){
-    document.getElementById("tailleErreur").innerHTML = "taille incorrect, veuillez donner une taille en cm";
-} else{document.getElementById("taille_erreur").innerHTML = "";
-resultasTaille = true;
-}
-if(poids === "" || isNaN(poids) || (poids <= 0)){
-    document.getElementById("poids_erreur").innerHTML = "poids incorrect, veuillez donner un poids en kg";
-} else{document.getElementById("poids_erreur").innerHTML = "";
-result_poids = true;
-}
-if(result_poids && result_taille){*/
     const imc = (poids/ ((taille * taille)/10000)).toFixed(2);
     if(imc < 18.5){
-        resultat.innerHTML = "Maigreur:" +""+ imc;
-    }else if(imc > 18.5 && imc <= 25){
-        resultat.innerHTML = "Normal:" +""+ imc;
-    }else if(imc > 25 && imc <= 30){
-        resultat.innerHTML = "En surpoids" +""+ imc;
-    } 
-    else{
-        resultat.innerHTML = "Obésité:" +""+ imc;
-    }
+        resultat.innerHTML = "Maigreur:" +" "+ imc;}
+    else if(imc > 18.5 && imc <= 25)
+    {resultat.innerHTML = "Normal:" +" "+ imc; }
+    else if(imc > 25 && imc <= 30){
+            resultat.innerHTML = "En surpoids" +" "+ imc;}  
+              else{
+        resultat.innerHTML = "Obésité:" +" "+ imc;}
    
- }});
+    };
+ 
+button = document.getElementById("imc");
+button.addEventListener('click', calculeImc);
