b28abafa5b59bfa0f6b7915d239851222fa0c090
f-ghalous <111303122+f-ghalous@users.noreply.github.com>
2022-10-26 16:49:42 -0400

    update

--- projet/java.js
+++ projet/java.js
@@ -6,17 +6,17 @@ function calculeImc(){
     let resultat = document.getElementById("output");
     const imc = (poids/ ((taille * taille)/10000)).toFixed(2);
     if(imc < 18.5){
-        resultat.innerHTML = "Maigreur:" +" "+ imc;}
+        resultat.innerHTML = "Maigreur:" + " " + imc;}
     else if(imc >= 18.5 && imc < 25)
-    {resultat.innerHTML = "Normal:" +" "+ imc; }
+    {resultat.innerHTML = "Normal:" + " " + imc; }
     else if(imc >= 25 && imc < 30){
-            resultat.innerHTML = "En surpoids" +" "+ imc;}  
+            resultat.innerHTML = "En surpoids" + " " + imc;}  
               else if(imc >= 30 && imc < 35){
-                resultat.innerHTML = "Obésité modérè:" +" "+ imc;}
+                resultat.innerHTML = "Obésité modérè:" + " " + imc;}
                 else if(imc >= 35 && imc < 40)
-                { resultat.innerHTML = "Obésité sévère:" +" "+ imc;}
+                { resultat.innerHTML = "Obésité sévère:" + " " + imc;}
                      else{
-                        resultat.innerHTML = "Obésité morbide:" +" "+ imc;
+                        resultat.innerHTML = "Obésité morbide:" + " " + imc;
                      }
    
     };
